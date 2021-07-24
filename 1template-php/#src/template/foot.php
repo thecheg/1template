47909193ea047f19a1a703ef9d6e82f381b53d86
Alexey Morozov <thecheg@yandex.ru>
2021-07-24 07:28:09 +0500

    fix

--- 1template-php/#src/template/foot.php
+++ 1template-php/#src/template/foot.php
@@ -1,8 +1,7 @@
 <?php include $rootPath.'template/svg-sprite.php';?>
 	<script>
 		// config
-		var rootPath = '<?=$rootPath;?>',
-			siteName = '<?=$siteName;?>';
+		var rootPath = '<?=$rootPath;?>';
 	</script>
 	<script src="<?=$rootPath;?>js/plugins.min.js?t=@@include('@@webRoot/data/timestamp.txt')"></script>
 	<script src="<?=$rootPath;?>js/scripts.min.js?t=@@include('@@webRoot/data/timestamp.txt')"></script>
